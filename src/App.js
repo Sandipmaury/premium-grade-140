8aed26ec524f0012f93cb41221825b4084595934
Ayush Anshu <ayushanshu56@gmail.com>
2022-11-10 04:28:12 +0530

    day_2 work-frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
+import Signup from "./routes/Signup";
 import { AllRoutes } from "./routes/AllRoutes";
 
 export default function App() {
   return (
     <Box w="100%">
       <AllRoutes />
+      <Signup/>
     </Box>
   );
 }
--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Signup from "./Signup";
 import { HomePage } from "./HomePage";
 
 export const AllRoutes = () => {
