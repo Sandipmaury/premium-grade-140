c480b0ebc11266ae0e186fb4df0c4614f2d4e0f7
AwsomeSwati <swati1993kushwaha@gmail.com>
2022-11-10 00:20:57 +0530

    slide-show of images added

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -6,8 +6,8 @@ import KindMoments from "./KindMoments";
 export const AllRoutes = () => {
   return (
     <Routes>
-      {/* <Route path="/" element={<HomePage />} /> */}
-      <Route path="/" element={<KindMoments />} />
+      <Route path="/" element={<HomePage />} />
+      {/* <Route path="/" element={<KindMoments />} /> */}
     </Routes>
   );
 };
