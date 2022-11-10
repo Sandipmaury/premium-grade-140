5a93bcf09ef3985c29e50f6893f217f5a8c268da
Ayush Anshu <ayushanshu56@gmail.com>
2022-11-10 10:12:28 +0530

    Use Navigate in Navbar

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./HomePage";
+import Signup from "./Signup";
 
 export const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 };
