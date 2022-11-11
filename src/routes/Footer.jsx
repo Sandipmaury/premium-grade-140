8ce8d96637680fff93377b3d91f40a6fef093321
AyeshaKhan14 <101391413+AyeshaKhan14@users.noreply.github.com>
2022-11-12 01:06:45 +0530

    footer nav

---
+++ src/routes/Footer.jsx
@@ -0,0 +1,100 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import { Flex, Image, Text } from "@chakra-ui/react";
+
+export const Footer = () => {
+  return (
+    <div style={{width:"100%", backgroundColor:"#2bb673"}}>
+        <Box h={"340px"} w={"100%"} bg={"#2bb673"} >
+            {/* 1st div */}
+            <Box w={"1100px"} m={"auto"} h={"260px"}  >
+                <Flex h={"100%"} >
+               
+               <Box w={"20%"} h={"100%"}>
+               <Box color={"#ffffff"} fontWeight={"bold"} >General</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"} fontFamily={"Arial"}>Home</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Sign Up</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Businesses / Restaurateurs</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Advertising</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>About KindMeal.my</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Help & FAQ</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Terms & Conditions</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Inspiring Partners</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Lifestyle Ambassadors</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Jobs & Careers</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Contact Us</Box>
+               </Box>
+
+
+               <Box  w={"20%"} h={"100%"}>
+               <Box color={"#ffffff"} fontWeight={"bold"} >Features</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"} fontFamily={"Arial"}>Meat-Free Deals</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Tasty Menus</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Kind Moments</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Meat-Free Recipes</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Member Recommendations</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Featured Restaurants</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Vegetarian & Vegan Directory</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Food Map</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Become A Superhero</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Vegan News & Vegetarian Articles</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Latest Comments</Box>
+               </Box>
+
+
+
+
+               <Box w={"20%"} h={"100%"}>
+               <Box color={"#ffffff"} fontWeight={"bold"} >Social Media</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"} fontFamily={"Arial"}>KindMeal Widget</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Facebook</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Twitter</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Instagaram</Box>
+               </Box>
+
+
+
+
+               <Box  w={"20%"} h={"100%"}>
+               <Box color={"#ffffff"} fontWeight={"bold"} >Mobile</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"} fontFamily={"Arial"}>iPhone & iPod App</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Android App</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Exciting Promos</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Gadhimai: Ending Mass Slaughter</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Free Meals</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Food Bloggers</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Uber CHIRP</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Jane Goodall Contest</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Win Digi iPhone 6</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Feed The Poor</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Win Superhero Gifts</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Win an iPad Mini 3</Box>
+               </Box>
+
+
+
+
+               <Box  w={"20%"} h={"100%"}>
+               <Box color={"#ffffff"} fontWeight={"bold"} >PetFinder.my</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"} fontFamily={"Arial"}>Adopt A Pet</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Smartphone Apps</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>WAGazine</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Discussion Forum</Box>
+               <Box fontWeight={"normal"} fontSize={"12px"} color={"#eeeeee"}>Medical Fund</Box>
+               </Box>
+                </Flex>
+
+            </Box>
+
+
+             {/* 2st div */}
+            <Box w={"1100px"} m={"auto"} marginTop={"10px"} h={"38px"}>
+                <Box textAlign={"center"} color={"#eeeeee"} fontSize={"11px"}>Copyright © KindMeal.my, 2014 - 2022. All rights reserved.</Box>
+         
+           <Box textAlign={"center"} color={"#eeeeee"} fontSize={"11px"}>This website promotes compassionate, meat-free dining experience. Some food may contain eggs, dairy products or alcohol, please view individual listings for details.</Box>
+            </Box> 
+
+        </Box>
+    </div>
+  )
+}
