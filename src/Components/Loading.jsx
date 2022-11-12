bdab89baa65430b20fc821b1f64dc555ecfe1a3c
AwsomeSwati <swati1993kushwaha@gmail.com>
2022-11-12 21:29:37 +0530

    loading component added

---
+++ src/Components/Loading.jsx
@@ -0,0 +1,17 @@
+import { Circles } from "react-loader-spinner";
+import { Flex, Modal, ModalContent, ModalOverlay } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+
+export const Loding = (isTrue) => {
+    const loading  =  useSelector((store) => store.recipeReducer.loading);
+  return (
+    <Modal isCentered isOpen={loading}>
+      <ModalOverlay />
+      <ModalContent bg="transparent" boxShadow="none">
+        <Flex justifyContent="center">
+          <Circles />
+        </Flex>
+      </ModalContent>
+    </Modal>
+  );
+};
